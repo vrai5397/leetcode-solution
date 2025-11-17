98c310b58c48f0bcdb0a34e2c72275913d5a4fab
vrai5397 <vrai5397@gmail.com>
2025-11-17 18:42:05 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (94.55%) - LeetSync

--- 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.cpp
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.cpp
@@ -2,23 +2,19 @@ class Solution {
 public:
 int mod=1e9+7;
     int numSub(string s) {
-        // for consecutive one contribution is (n+1)*n/2
-        int i=0;
         int n=s.size();
-        int count=0;
-        while(i<n){
-    long long j = i;
-            while (j < n && s[j] == '1') j++;
+        long long count=0;
+        for(int i=0;i<n;i++){
+           if(s[i]=='0') continue;
+            long long j=i;
+            while(j<n&& s[j]!='0')
+            j++;
+        long long ones=j-i;
+        long long contri=(ones*(ones+1))/2;
+        count=(count+contri)%mod;
+         i=j;
 
-            long long ones = j - i;
-
-            // compute ones * (ones + 1) / 2 safely
-            long long contrib = (ones * (ones + 1) / 2) % mod;
-
-            count = (count + contrib) % mod;
-
-            i = j+1;  // go to first zero
         }
-        return count;
+        return (int) count;
     }
 };

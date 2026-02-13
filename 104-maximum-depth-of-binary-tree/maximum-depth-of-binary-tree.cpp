26458540e521d8612b6f175e92e15630e60276de
vrai5397 <vrai5397@gmail.com>
2026-02-13 22:23:03 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (96.33%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -1,37 +1,17 @@
 
 class Solution {
 public:
-int level_order(TreeNode* root){
-   
-    queue<TreeNode*> q;
-    q.push(root);
-    q.push(NULL); // we know that one level is completed
-    int level=0;
-    while(!q.empty()){
-        TreeNode* temp=q.front();
-        q.pop();
-        if(temp==NULL){
-           level+=1;
-           if(!q.empty())
-           q.push(NULL);
-        }
-        else{
-            if(temp->left)
-            q.push(temp->left);
-            if(temp->right)
-            q.push(temp->right);
-        }
-    }
+int inorder(TreeNode* root){
+    // base case
+    if(root==NULL)
+    return 0;
 
-    return level;
+    int left=1+inorder(root->left);
+    int right=1+inorder(root->right);
+
+    return max(left,right);
 }
     int maxDepth(TreeNode* root) {
-        // we can do using level order traversal
-         if(root==NULL)
-       return 0;
-
-       int ans=level_order(root);
-       return ans;
-
+        return inorder(root);
     }
 };

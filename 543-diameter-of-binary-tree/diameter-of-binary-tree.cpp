78f1ae25238992597ca678ed9794ef01f810eb96
vrai5397 <vrai5397@gmail.com>
2026-02-14 00:29:06 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (72.39%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -1,36 +1,25 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
-int height_tree(TreeNode* root){
-    // base case
-    if(root==NULL)
-    return 0;
+    pair<int,int> diameter(TreeNode* root){
+        if(root == NULL)
+            return {0,0};
 
-    int left=height_tree(root->left);
-    int right=height_tree(root->right);
-    return 1+max(left,right);
-}
+        pair<int,int> left = diameter(root->left);
+        pair<int,int> right = diameter(root->right);
 
-int diameter(TreeNode* root){
-    // base case
-if(root==NULL)
-return 0;
-    int root_diameter=height_tree(root->left)+height_tree(root->right);
- int max_diameter=max(root_diameter,max(diameter(root->left),diameter(root->right)));
- return max_diameter;
-}
+        int left_diam = left.first;
+        int right_diam = right.first;
+
+        int curr_height = 1 + max(left.second, right.second);
+
+        int curr_diam = max(left.second + right.second,
+                       max(left_diam, right_diam));
+
+        return {curr_diam, curr_height};
+    }
+
     int diameterOfBinaryTree(TreeNode* root) {
-         // recursion ke ander recursion
-return diameter(root);
+        return diameter(root).first;
     }
 };

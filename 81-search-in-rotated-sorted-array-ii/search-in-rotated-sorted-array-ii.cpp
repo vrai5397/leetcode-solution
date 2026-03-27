58de3671fd195d01ae29cdd246f6f43c589c8397
vrai5397 <vrai5397@gmail.com>
2026-03-27 14:41:31 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (23.80%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int lo = 0, hi = n - 1;
+        // steps 
+        // 1---> find the sorted half
+        // 2---> in which half i have to serach
+        int n=nums.size();
+        int lo=0;
+        int hi=n-1;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(nums[mid]==target)
+            return true;
+    else if(nums[lo]==nums[mid]&&nums[mid]==nums[hi]){
+          // trim the space 
+          lo=lo+1;
+          hi=hi-1;
+          continue;
+    }
 
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
+    else if(nums[lo]<=nums[mid]){
+        // left half is sorted
+        if(nums[lo]<=target&&target<=nums[mid])
+        hi=mid-1;
 
-            if (nums[mid] == target)
-                return true;
-    else  if(nums[lo]==nums[mid]&&nums[mid]==nums[hi]){
-                    // we have to trim
-                    lo=lo+1;
-                    hi=hi-1;
-                    continue;
-                }
+        else 
+        lo=mid+1;
+    }
+    else{
+        if(nums[mid]<=target&&target<=nums[hi])
+        lo=mid+1;
+        else
+        hi=mid-1;
+    }
 
-            // Left half sorted
-            if (nums[lo] <= nums[mid]) {
-                if (nums[lo] <= target && target < nums[mid])
-                    hi = mid - 1;
-                else
-                    lo = mid + 1;
-            }
-            // Right half sorted
-            else {
-                if (nums[mid] < target && target <= nums[hi])
-                    lo = mid + 1;
-                else
-                    hi = mid - 1;
-            }
+
         }
         return false;
     }

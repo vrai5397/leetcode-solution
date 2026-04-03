58308e5786e3b4bd68bd93ce0de50258cdb5507d
vrai5397 <vrai5397@gmail.com>
2026-04-03 18:05:51 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (24.46%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        // steps 
-        // 1---> find the sorted half
-        // 2---> in which half i have to serach
+        // we also have duplictae element
         int n=nums.size();
         int lo=0;
         int hi=n-1;
@@ -11,29 +9,27 @@ public:
             int mid=lo+(hi-lo)/2;
             if(nums[mid]==target)
             return true;
-    else if(nums[lo]==nums[mid]&&nums[mid]==nums[hi]){
+     else if(nums[lo]==nums[mid]&&nums[mid]==nums[hi]){
           // trim the space 
           lo=lo+1;
           hi=hi-1;
           continue;
-    }
-
-    else if(nums[lo]<=nums[mid]){
-        // left half is sorted
-        if(nums[lo]<=target&&target<=nums[mid])
-        hi=mid-1;
-
-        else 
-        lo=mid+1;
-    }
-    else{
-        if(nums[mid]<=target&&target<=nums[hi])
-        lo=mid+1;
-        else
-        hi=mid-1;
-    }
-
-
+     }
+    
+            else if(nums[mid]>=nums[lo]){
+                // left half is sorted
+                if(nums[lo]<=target&&target<nums[mid])
+                hi=mid-1;
+                else
+                lo=mid+1;
+            }
+              else{
+                // right half is sorted
+                if(nums[mid]<target&&target<=nums[hi])
+                lo=mid+1;
+                else
+                hi=mid-1;
+            }
         }
         return false;
     }

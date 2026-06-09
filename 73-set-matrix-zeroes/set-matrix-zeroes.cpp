0851b23dc8eab31b2fa4dc01e24734df87f832f1
vrai5397 <vrai5397@gmail.com>
2026-06-09 11:55:04 +0530

    Time: 31 ms (8.66%) | Memory: 24.1 MB (5.10%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
-void setzero(vector<vector<int>>& matrix1,int i,int j){
-    int r=matrix1.size();
-    int c=matrix1[0].size();
-    // row
-   for(int k=0;k<c;k++)
-   matrix1[i][k]=0;
-   // col
-   for(int k=0;k<r;k++ )
-   matrix1[k][j]=0;
+void setzero(vector<vector<int>>& matrix,int i,int j){
+ 
+     int r=matrix.size();
+    int c=matrix[0].size();
+    for(int k=0;k<c;k++){
+        matrix[i][k]=0;
+    }
+     for(int k=0;k<r;k++){
+        matrix[k][j]=0;
+    }
 }
     void setZeroes(vector<vector<int>>& matrix) {
-       // brute force 
-       int r=matrix.size();
-       int c=matrix[0].size();
-       vector<vector<int>> matrix1(r,vector<int>(c,0));
-       for(int i=0;i<r;i++){
-        for(int j=0;j<c;j++){
-            matrix1[i][j]=matrix[i][j];
-        }
-       }
-       for(int i=0;i<r;i++){
-        for(int j=0;j<c;j++){
-            if(matrix[i][j]==0){
-                setzero(matrix1,i,j);
+        map<int,pair<int,int>> mp;
+        int r=matrix.size();
+        int c=matrix[0].size();
+        int x=0;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(matrix[i][j]==0){
+          mp.insert(make_pair(x, make_pair(i, j)));
+          x++;
+                }
             }
         }
-       }
-        for(int i=0;i<r;i++){
-        for(int j=0;j<c;j++){
-            matrix[i][j]=matrix1[i][j];
+        for(auto x:mp){
+          setzero(matrix,x.second.first,x.second.second);
         }
-       }
     }
 };

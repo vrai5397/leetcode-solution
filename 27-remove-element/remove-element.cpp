287c9723605c1b6044296592f1af009ec14eb7c8
vrai5397 <vrai5397@gmail.com>
2026-08-02 11:34:10 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (49.40%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-           vector<int> ans;
-        int count=0;
+        int k=0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]!=val){
-            ans.push_back(nums[i]);
-          
-        }
-          else count++;
-        }
-        for(int i=0;i<count;i++)
-        ans.push_back(val);
+                nums[k]=nums[i];
+                k++;
+            }
 
-        
-        nums=ans;
-
-        return nums.size()-count;
+        }
+        return k;
     }
 };

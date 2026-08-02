54af6a1447ff558771add2133ce58784b3e919c5
vrai5397 <vrai5397@gmail.com>
2026-08-02 10:56:16 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (40.07%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-      // brute force
-      vector<int> ans;
-      for(int i=0;i<nums.size()-1;i++){
-        for(int j=i+1;j<nums.size();j++){
-            if(nums[i]+nums[j]==target){
-                ans.push_back(i);
-                ans.push_back(j);
+        unordered_map<int,int> mp;
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+            if(mp.find(target-nums[i])!=mp.end()){
+               ans.push_back(i);
+               ans.push_back(mp[target-nums[i]]);
+               break;
             }
+            else
+            mp[nums[i]]=i;
         }
-      }
-return ans;
+        return ans;
     }
 };

f9d1beb9714e6e84a765d678c011bf9f41fc8731
vrai5397 <vrai5397@gmail.com>
2026-08-02 11:44:07 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (56.59%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-         // brute force
-         vector<int> ans;
-         int count=0;
-         for(int i=0;i<nums.size();i++){
-            if(nums[i]!=0)
-            ans.push_back(nums[i]);
-            else
-            count++;
-         }
-         for(int i=0;i<count;i++)
-         ans.push_back(0);
-         nums=ans;
+        int k=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[k]);
+                k++;
+            }
+        }
     }
 };

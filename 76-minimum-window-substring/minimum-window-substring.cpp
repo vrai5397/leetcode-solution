4b98af01ebcc0ad4ff6af48320632333cde3ab20
vrai5397 <vrai5397@gmail.com>
2026-08-13 12:24:25 +0530

    Time: 18 ms (25.09%) | Memory: 11.5 MB (43.40%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -23,8 +23,8 @@ int freqcount(unordered_map<char,int>& mp){
                 minlen=j-i+1;
              }
              mp[s[i]]++;
-             if(mp[s[i]]==0)
-             mp.erase(s[i]);
+            //  if(mp[s[i]]==0)
+            //  mp.erase(s[i]);
              i++;
           }
 j++;

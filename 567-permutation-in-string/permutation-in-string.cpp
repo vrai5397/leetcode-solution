d12d65290b7ca90983e2124e4ea92cdc63d21444
vrai5397 <vrai5397@gmail.com>
2026-08-13 11:04:53 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (45.12%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
-bool checkzero(vector<int> freq){
+bool freqcount(vector<int>& freq){
     for(int i=0;i<26;i++){
-        if(freq[i]!=0) return false;
-       
+    if(freq[i]!=0) return false;
     }
-    return true;
+return true;
+
 }
     bool checkInclusion(string s1, string s2) {
-        //suppose we have window of s1 size
-        // find the window of that size in s2 with same alphabet
-         vector<int> freq(26,0);
-         int k=s1.size();
-         for(int i=0;i<s1.size();i++){
-            freq[s1[i]-'a']++;
-         }
-         // sliding window
-         int i=0;
-         int j=0;
-         while(j<s2.size()){
-             freq[s2[j]-'a']--;
-             while(j-i+1>k){
-                freq[s2[i]-'a']++;
-                i++;
-             }
-
-             if(j-i+1==k&& checkzero(freq))
-             return true;
-
-             j++;
-         }
-         return false;
+        //we have to find string of ab length in s2
+        // continious manner
+        // substring s1 size
+       // sliding window
+       int i=0;
+       int j=0;
+       vector<int> freq(26,0);
+       for(int i=0;i<s1.size();i++)
+       freq[s1[i]-'a']++;
+    int k=s1.size();
+       while(j<s2.size()){
+          freq[s2[j]-'a']--;
+           while(j-i+1>k){
+              freq[s2[i]-'a']++;
+              i++;
+           }
+           if(j-i+1==k&& freqcount(freq))
+           return true;
+           j++;
+       }
+       return false;
     }
 };

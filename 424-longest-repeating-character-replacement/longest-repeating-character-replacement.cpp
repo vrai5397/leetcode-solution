237965b3e99b0efae5b6801a058fe8ed523b700f
vrai5397 <vrai5397@gmail.com>
2026-08-13 11:28:50 +0530

    Time: 2 ms (71.51%) | Memory: 11.4 MB (5.39%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-       // brute force
-       // we have to find substring with at most
-       // k diff character 
-       int i=0;
-       int j=0;
-       unordered_map<char,int> mp;
-       int maxlen=0;
-       int maxfreq=-1e9;
-       while(j<s.size()){
-        mp[s[j]]++;
-          for(auto x:mp){
-             maxfreq=max(maxfreq,x.second);
-          }
-          while((j-i+1)-maxfreq>k){
+        // j-i+1-max
+        int i=0;
+        int j=0;
+        unordered_map<char,int> mp;
+        int maxfreq=0;
+        int maxlen=0;
+        while(j<s.size()){
+             mp[s[j]]++;
+           for(auto x:mp)
+           maxfreq=max(maxfreq,x.second);
+           // remaining chracter
+           while((j-i+1)-maxfreq>k){
               mp[s[i]]--;
               if(mp[s[i]]==0)
-              mp.erase(mp[s[i]]);
+              mp.erase(s[i]);
               i++;
-          }
-          maxlen=max(maxlen,j-i+1);
-          j++;
-       }
-       return maxlen;
+           }
+           maxlen=max(maxlen,j-i+1);
+           j++;
+        }
+        return maxlen; 
     }
 };

536e3320644350bf24ae28081802d268ab9ebe64
vrai5397 <vrai5397@gmail.com>
2026-08-13 12:04:47 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (64.55%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     int maxScore(vector<int>& nums, int k) {
-        int n=nums.size();
-        // n-k
-        // 22
-        // tell the window of n-k size with minimum sum
-        int totalsum=0;
-        for(int i=0;i<nums.size();i++){
-            totalsum+=nums[i];
-        } 
-
-        int i=0;
-        int j=0;
-        int sum=0;
-        int minsum=1e9;
-        while(j<nums.size()){
-            sum+=nums[j];
-            while(j-i+1>n-k){
-               sum-=nums[i];
-               i++;
-            }
-            if(j-i+1==n-k)
-            minsum=min(minsum,sum);
-            j++;
-        }  
-
-        return totalsum-minsum;
+      
+      // we have to find subarray of size n-k with minsum
+      int totalsum=0;
+       for(int i=0;i<nums.size();i++)
+       totalsum+=nums[i];
+       int i=0;
+       int j=0;
+       int n=nums.size();
+       int sum=0;
+       int minsum=1e9;
+       
+      
+       while(j<nums.size()){
+           sum+=nums[j];
+           while(j-i+1>n-k){
+              sum-=nums[i];
+              i++;
+           }
+           if(j-i+1==n-k)
+           minsum=min(minsum,sum);
+           j++;
+       }
+       return totalsum-minsum;
     }
 };

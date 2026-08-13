d59b312efd3749b38074c9e668368f6452f25212
vrai5397 <vrai5397@gmail.com>
2026-08-13 11:46:16 +0530

    Time: 45 ms (50.02%) | Memory: 84.7 MB (92.45%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    int totalFruit(vector<int>& fruits) {
-        // we have to find subarray with 2 diff number
-        if(fruits.size()==1) return fruits.size();
+    int totalFruit(vector<int>& nums) {
+        // maxlen of subarray with two character
+        if(nums.size()==1) return 1;
         int i=0;
-        int j=i;
+        int j=0;
         unordered_map<int,int> mp;
         int maxcount=0;
-        while(j<fruits.size()){
-            mp[fruits[j]]++;
-            while(mp.size()>2){
-                mp[fruits[i]]--;
-                if(mp[fruits[i]]==0)
-                mp.erase(fruits[i]);
+        while(j<nums.size()){
+           mp[nums[j]]++;
+           while(mp.size()>2){
+                mp[nums[i]]--;
+                if(mp[nums[i]]==0)
+                mp.erase(nums[i]);
                 i++;
-            }
-           
-            maxcount=max(maxcount,j-i+1);
-            j++;
+           }
+           maxcount=max(maxcount,j-i+1);
+           j++;
         }
         return maxcount;
     }

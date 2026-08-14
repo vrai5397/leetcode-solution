aa98f4e7bbf7093085d43f4ef0945e60664e6d56
vrai5397 <vrai5397@gmail.com>
2026-08-14 12:29:18 +0530

    Time: 3 ms (8.50%) | Memory: 263.5 MB (5.73%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-void reverse(vector<int>& nums,int i,int j){
-    // swap karte jana hai
-    while(i<=j){
+void reversearr(vector<int>& nums,int i,int j){
+    while(i<j){
         swap(nums[i],nums[j]);
         i++;
         j--;
     }
 }
     void rotate(vector<int>& nums, int k) {
-        // reverse full array
-        // reverse 0 to k-1
-        // k to n-1
         //7 6 5 4 3 2 1
-        // 5 6 7 1 2 3 4
-        if(nums.size()==1)
-        return;
-        k=k%nums.size();
-        reverse(nums,0,nums.size()-1);
-        reverse(nums,0,k-1);
-        reverse(nums,k,nums.size()-1);
+        //0 to k-1
+        // k to n-1
+        
+        int n=nums.size();
+         k=k%n;
+      reversearr(nums,0,n-1);
+      reversearr(nums,0,k-1);
+      reversearr(nums,k,n-1);
     }
 };

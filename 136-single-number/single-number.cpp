21a8cd3051a4d6c309d695f0033d61b861d20cc4
vrai5397 <vrai5397@gmail.com>
2026-08-15 11:49:43 +0530

    Time: 10 ms (14.30%) | Memory: 24 MB (7.47%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++){
+              mp[nums[i]]++;
+            
+        }
+        int ans=0;
+        for(auto x:mp){
+            if(x.second==1)
+            ans=x.first;
+        }
+        return ans;
+    }
+};

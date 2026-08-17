0aa0086d192070e6dae4a818d5ed9e0fb8efda8c
vrai5397 <vrai5397@gmail.com>
2026-08-17 17:04:25 +0530

    Time: 0 ms (100.00%) | Memory: 32.7 MB (63.44%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-// code to find subarray with <=goal
 int solve(vector<int>& nums,int goal){
     if(goal<0) return 0;
     int i=0;
@@ -8,19 +7,19 @@ int solve(vector<int>& nums,int goal){
     int sum=0;
     int count=0;
     while(j<nums.size()){
-      sum+=nums[j];
-      while(sum>goal){
-         sum-=nums[i];
-         i++;
-      }
-       count+=j-i+1;
-       j++;
+        sum+=nums[j];
+         while(sum>goal){
+           sum-=nums[i];
+           i++;
+         }
+         count+=j-i+1;
+         j++;
     }
     return count;
 }
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        // simple formaula
-        // atmost(goal)-atmost(goal-1)=exact(goal)
-         return solve(nums,goal)-solve(nums,goal-1);
+        // sliding window concept
+        // atmost(k)-atmost(k-1)
+        return solve(nums,goal)-solve(nums,goal-1);
     }
 };

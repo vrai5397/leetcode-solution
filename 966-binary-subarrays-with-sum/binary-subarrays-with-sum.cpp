ff5eca4ce249170789927bc6b6b3feb3c5634903
vrai5397 <vrai5397@gmail.com>
2026-08-17 17:06:55 +0530

    Time: 20 ms (24.78%) | Memory: 38.7 MB (26.79%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
-int solve(vector<int>& nums,int goal){
-    if(goal<0) return 0;
-    int i=0;
-    int j=0;
-    int sum=0;
-    int count=0;
-    while(j<nums.size()){
-        sum+=nums[j];
-         while(sum>goal){
-           sum-=nums[i];
-           i++;
-         }
-         count+=j-i+1;
-         j++;
-    }
-    return count;
-}
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        // sliding window concept
-        // atmost(k)-atmost(k-1)
-        return solve(nums,goal)-solve(nums,goal-1);
+        // count subarray
+        // prefix sum
+        unordered_map<int,int> mp;
+        mp[0]++;
+        int count=0;
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            if(mp.find(sum-goal)!=mp.end()){
+                count+=mp[sum-goal];
+            }
+            mp[sum]++;
+        }
+        return count;
     }
 };

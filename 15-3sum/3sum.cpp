a3912985cd23809a282446bbbd14060823361da1
vrai5397 <vrai5397@gmail.com>
2026-08-18 17:03:23 +0530

    Time: 62 ms (16.89%) | Memory: 34.8 MB (12.50%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,38 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        // first we have to sort
-        // when asked about index we cannot sort
-        // when asked about value we can sort
-       sort(nums.begin(),nums.end());
-       vector<vector<int>> ans;
-       for(int i=0;i<nums.size();i++){
-          if(i>0 && nums[i]==nums[i-1])
-          continue;
-          int target=-nums[i];
-          int j=i+1;
-          int k=nums.size()-1;
-          while(j<k){
-        if(nums[j]+nums[k]>target)
-        k--;
-        else if(nums[j]+nums[k]<target)
-        j++;
-        else{ // equal target
-            while(j<k&&nums[j]==nums[j+1]) // skip duplicate
-            j++;
-            while(j<k&&nums[k]==nums[k-1]) // skip duplicate
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+       for(int i=0;i<nums.size()-2;i++){
+        if(i > 0 && nums[i] == nums[i-1])
+    continue;
+         int target=-nums[i];
+         unordered_map<int,int> mp;
+         int j=i+1;
+         int k=nums.size()-1;
+         while(j<k){
+            if(nums[j]+nums[k]>target)
             k--;
-            vector<int> helper;
+           else if(nums[j]+nums[k]<target)
+           j++;
+           else{
+             while(j<k&&nums[j]==nums[j+1])
+             j++;
+             while(j<k&&nums[k]==nums[k-1])
+             k--;
+               vector<int> helper;
             helper.push_back(nums[i]);
             helper.push_back(nums[j]);
             helper.push_back(nums[k]);
             ans.push_back(helper);
-            j++;
-            k--;
-
-        }
-          }
-       }
+             j++;
+             k--;
+           }
+         }
+       } 
        return ans;
     }
 };

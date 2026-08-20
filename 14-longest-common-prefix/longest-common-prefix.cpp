e1ded81d10475bca22e0a5b2ad2c1551dbf08bab
vrai5397 <vrai5397@gmail.com>
2026-08-20 11:42:37 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (38.83%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -4,7 +4,7 @@ public:
         string ans="";
        for(int i=0;i<strs[0].size();i++){
          for(int j=0;j<strs.size()-1;j++){
-            if(strs[j][i]!=strs[j+1][i])
+        if(i>=strs[j].size()||i>=strs[j+1].size()||strs[j][i]!=strs[j+1][i])
             return ans;
            
          }

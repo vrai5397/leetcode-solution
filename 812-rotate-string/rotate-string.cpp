0bb6ac4adb8a9f721a240489fe9ad8986dd9c89b
vrai5397 <vrai5397@gmail.com>
2026-08-20 16:27:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (58.12%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -5,27 +5,8 @@ public:
         return false;
         string ans=s;
         ans+=s;
-        int j=0;
-        int i=0;
+       
      
-      while(i<ans.size()){
-        if(ans[i]==goal[j]){
-            // we have to check
-              int p=i;
-            for(int k=j;k<goal.size();k++){
-
-              
-                if(ans[p]!=goal[k])
-                break;
-
-          else if(k==goal.size()-1&&ans[p]==goal[k])
-            return true;
-
-        p++;
-            }
-        }
-        i++;
-      }
-      return false;
+    return ans.find(goal)!=string::npos;
     }
 };

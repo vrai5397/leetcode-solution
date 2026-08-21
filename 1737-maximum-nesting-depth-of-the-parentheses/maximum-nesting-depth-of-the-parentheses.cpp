ee816b6a8ad816da4a085ce2f723c92eabfd6ef5
vrai5397 <vrai5397@gmail.com>
2026-08-21 11:11:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (24.76%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -3,10 +3,9 @@ public:
     int maxDepth(string s) {
         int left=0;
         int right=0;
-        if(s[0]=='(')
-        left+=1;
+      
         int maxdepth=0;
-        for(int i=1;i<s.size();i++){
+        for(int i=0;i<s.size();i++){
             int depth=left-right;
             maxdepth=max(maxdepth,depth);
             if(s[i]=='(')

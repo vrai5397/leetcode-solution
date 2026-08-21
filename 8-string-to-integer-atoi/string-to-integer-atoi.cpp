9169962930aa06568b2288b5b259d6bb72f42340
vrai5397 <vrai5397@gmail.com>
2026-08-21 12:11:14 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (9.08%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,43 @@
+
+class Solution {
+public:
+    int myAtoi(string s) {
+        if (s.size() == 0)
+            return 0;
+
+        int i = 0;
+
+        while (i < s.size() && s[i] == ' ')
+            i++;
+
+        string st = s.substr(i, s.size());
+
+        int sign = 1;
+        long long ans = 0;
+
+        for (int j = 0; j < st.size(); j++) {
+
+            if (j == 0 && st[j] == '-') {
+                sign = -1;
+            }
+            else if (j == 0 && st[j] == '+') {
+                sign = 1;
+            }
+            else if (!isdigit(st[j])) {
+                break;
+            }
+            else {
+                ans = ans * 10 + (st[j] - '0');
+
+                // overflow check
+                if (sign == 1 && ans > INT_MAX)
+                    return INT_MAX;
+
+                if (sign == -1 && -ans < INT_MIN)
+                    return INT_MIN;
+            }
+        }
+
+        return ans * sign;
+    }
+};

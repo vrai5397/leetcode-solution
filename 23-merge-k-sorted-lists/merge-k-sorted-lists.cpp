7c46fa7bdea74be67475e6a185099205905ced1e
vrai5397 <vrai5397@gmail.com>
2026-08-23 12:39:44 +0530

    Time: 3 ms (61.24%) | Memory: 18.7 MB (39.53%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -1,56 +1,35 @@
-ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-
-    ListNode* dummy = new ListNode(-1);
-    ListNode* temp = dummy;
-
-    ListNode* temp1 = list1;
-    ListNode* temp2 = list2;
-
-    while(temp1 != NULL && temp2 != NULL) {
-
-        if(temp1->val <= temp2->val) {
-            temp->next = temp1;
-            temp1 = temp1->next;
-            temp = temp->next;
-        }
-        else {
-            temp->next = temp2;
-            temp2 = temp2->next;
-            temp = temp->next;
-        }
-    }
-
-    while(temp1 != NULL) {
-        temp->next = temp1;
-        temp1 = temp1->next;
-        temp = temp->next;
-    }
-
-    while(temp2 != NULL) {
-        temp->next = temp2;
-        temp2 = temp2->next;
-        temp = temp->next;
-    }
-
-    dummy = dummy->next;
-    return dummy;
-}  // ✅ close mergeTwoLists
-
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-
-          if(lists.size() == 0)
-            return NULL;
-
-        ListNode* ans = lists[0];
-
-        for(int i = 1; i < lists.size(); i++) {
-            ans = mergeTwoLists(ans, lists[i]);
+        // solving this question using priority_queue
+        priority_queue<pair<int,ListNode*>,vector<pair<int,ListNode*>>,
+        greater<pair<int,ListNode*>>> pq;
+        for(int i=0;i<lists.size();i++){
+            if(lists[i]!=NULL)
+            pq.push({lists[i]->val,lists[i]});
         }
-
-        return ans;
+           ListNode* dummy=new ListNode(-1);
+           ListNode* temp=dummy;
+            while(!pq.empty()){
+                auto x=pq.top();
+                pq.pop();
+                temp->next=x.second;
+               if(x.second->next != NULL) {
+                pq.push({x.second->next->val, x.second->next});
+            }
+temp=temp->next;
+            }
+        
+        return dummy->next;
     }
 };

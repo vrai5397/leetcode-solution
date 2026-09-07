a829d26505e96906be0a50e6e7a1342225d3aec1
vrai5397 <vrai5397@gmail.com>
2026-09-08 00:06:02 +0530

    Time: 0 ms (100.00%) | Memory: 71.7 MB (81.22%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        // largest sum
-        // kadane algo
+        if(nums.size()==1)
+        return nums[0];
         int currsum=0;
-        int maxsum=nums[0];
+        int maxsum=INT_MIN;
         for(int i=0;i<nums.size();i++){
             currsum+=nums[i];
             maxsum=max(maxsum,currsum);
             if(currsum<0)
-                currsum=0;
-            
+            currsum=0;
         }
-        return maxsum;
+return maxsum;
     }
 };

d3fc38005010b174324752b378a2487cf7e00026
vrai5397 <vrai5397@gmail.com>
2026-09-09 14:09:32 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (46.27%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
-bool freqcount(vector<int>& freq){
-    for(int i=0;i<26;i++){
-    if(freq[i]!=0) return false;
+bool count(vector<int>& freq){
+    for(auto x:freq){
+        if(x!=0)
+        return false;
     }
-return true;
-
+    return true;
 }
     bool checkInclusion(string s1, string s2) {
-        //we have to find string of ab length in s2
-        // continious manner
-        // substring s1 size
-       // sliding window
-       int i=0;
-       int j=0;
-       vector<int> freq(26,0);
-       for(int i=0;i<s1.size();i++)
-       freq[s1[i]-'a']++;
-    int k=s1.size();
-       while(j<s2.size()){
-          freq[s2[j]-'a']--;
-           while(j-i+1>k){
-              freq[s2[i]-'a']++;
-              i++;
-           }
-           if(j-i+1==k&& freqcount(freq))
+        int k=s1.size();
+        vector<int> freq(26,0);
+        for(int i=0;i<s1.size();i++){
+            freq[s1[i]-'a']++;
+        }
+        int i=0;
+        int j=0;
+        while(j<s2.size()){
+           freq[s2[j]-'a']--;
+            while(j-i+1>k){
+                freq[s2[i]-'a']++;
+                i++;
+            }
+           if(j-i+1==k&& count(freq))
            return true;
            j++;
-       }
-       return false;
+        }
+        return false;
     }
 };

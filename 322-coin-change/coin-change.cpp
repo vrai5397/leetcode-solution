de1dd32f8b445cfe45e3130c3b3e0136a9b38bcd
vrai5397 <vrai5397@gmail.com>
2026-09-10 01:22:36 +0530

    Time: 39 ms (48.49%) | Memory: 48.8 MB (45.20%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
-int solve(vector<int>& coins,int amount,int i,  vector<vector<int>>& dp){
-    if(amount==0)
-    return 0;
-    if(i==0){
-        if(amount%coins[i]==0)
-        return dp[i][amount]=amount/coins[i];
-        else return dp[i][amount]=1e9;
-    }
-    if(dp[i][amount]!=-1)
-    return dp[i][amount];
-    int exclude=solve(coins,amount,i-1,dp);
-    int include=1e9;
-    if(coins[i]<=amount){
-        include=1+solve(coins,amount-coins[i],i,dp);
-    }
-    return dp[i][amount]=min(exclude,include);
-}
+
     int coinChange(vector<int>& coins, int amount) {
-        // pick not pick
-        if(amount==0)
-        return 0;
-        vector<vector<int>> dp(coins.size(),vector<int>(amount+1,-1));
-        int ans= solve(coins,amount,coins.size()-1,dp);
        
-        if(ans==1e9)
-        return -1;
-        return ans;
-        
-      
+        vector<vector<int>> dp(coins.size(),vector<int>(amount+1,1e9));
+        int n=coins.size();
+           for(int i = 0; i < n; i++)
+            dp[i][0] = 0;
+            for(int i=1;i<=amount;i++){
+                  if(i%coins[0]==0)
+                  dp[0][i]=i/coins[0];
+            }
+           for(int i=1;i<n;i++){
+            for(int j=1;j<=amount;j++){
+                int exclude=dp[i-1][j];
+    int include=1e9;
+    if(coins[i]<=j)
+    include=1+dp[i][j-coins[i]];
+    dp[i][j]=min(include,exclude);
+            }
+    }
+      int ans=dp[coins.size()-1][amount];
+      if(ans==1e9)
+      return -1;
+      return ans;
     }
 };

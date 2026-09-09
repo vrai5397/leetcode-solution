6fe20e114a5aba7147bfd023b1455c9960152632
vrai5397 <vrai5397@gmail.com>
2026-09-09 14:22:43 +0530

    Time: 15 ms (17.22%) | Memory: 11.4 MB (5.15%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        // j-i+1-max
+        int maxfreq=0;
         int i=0;
         int j=0;
         unordered_map<char,int> mp;
-        int maxfreq=0;
         int maxlen=0;
         while(j<s.size()){
-             mp[s[j]]++;
+           mp[s[j]]++;
            for(auto x:mp)
            maxfreq=max(maxfreq,x.second);
-           // remaining chracter
-           while((j-i+1)-maxfreq>k){
+           while(j-i+1-maxfreq>k){
               mp[s[i]]--;
               if(mp[s[i]]==0)
               mp.erase(s[i]);
@@ -21,6 +19,6 @@ public:
            maxlen=max(maxlen,j-i+1);
            j++;
         }
-        return maxlen; 
+        return maxlen;
     }
 };

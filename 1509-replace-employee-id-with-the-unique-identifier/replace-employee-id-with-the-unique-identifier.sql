6ee5ff68684acfdaa1471825b14b65899f696c39
vrai5397 <vrai5397@gmail.com>
2026-09-18 11:12:26 +0530

    Time: 1319 ms (61.18%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-select EmployeeUNI.unique_id,Employees.name
-from Employees left join EmployeeUNI on
-Employees.id=EmployeeUNI.id;
+select eu.unique_id,e.name from
+Employees e 
+left join EmployeeUNI eu on e.id=eu.id

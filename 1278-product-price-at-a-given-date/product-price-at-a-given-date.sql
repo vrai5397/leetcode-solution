767767faf38ee9dd7a74b50cf350dccfd80cf077
vrai5397 <vrai5397@gmail.com>
2026-09-21 12:22:14 +0530

    Time: 487 ms (83.50%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,21 +1,18 @@
 # Write your MySQL query statement below
-SELECT product_id, new_price AS price
-FROM Products p
-WHERE (product_id, change_date) IN (
-    SELECT product_id, MAX(change_date)
-    FROM Products
-    WHERE change_date <= '2019-08-16'
-    GROUP BY product_id
+select product_id,new_price as price from
+Products
+where (product_id,change_date) in(
+    select product_id,max(change_date)
+    from Products where change_date<='2019-08-16'
+    group by product_id
 )
 
-UNION
+union
 
-SELECT product_id, 10 AS price
-FROM Products
-WHERE product_id NOT IN (
-    SELECT product_id
-    FROM Products
-    WHERE change_date <= '2019-08-16'
+select product_id,10 as  price 
+from Products
+where (product_id) not in(
+    select product_id
+    from Products where change_date<='2019-08-16'
+    
 )
-
-ORDER BY product_id;

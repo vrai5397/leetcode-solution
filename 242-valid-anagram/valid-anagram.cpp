ff2c4e66fef06d1305ba01c93a1cad547bd4b07b
vrai5397 <vrai5397@gmail.com>
2026-09-22 23:58:35 +0530

    Time: 7 ms (18.26%) | Memory: 9.6 MB (91.89%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,15 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size()!=t.size())
-        return false;
-        vector<int> freq1(26,0);
-        vector<int> freq2(26,0);
-        for(int i=0;i<s.size();i++){
-            freq1[s[i]-'a']++;
-            freq2[t[i]-'a']++;
-
-        }
-        return freq1==freq2;
+      sort(s.begin(),s.end());
+      sort(t.begin(),t.end());
+      return s==t;  
     }
 };

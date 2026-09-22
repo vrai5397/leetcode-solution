bae041e04bf6681149b0a15196949081fc079fdd
vrai5397 <vrai5397@gmail.com>
2026-09-23 00:32:28 +0530

    Time: 76 ms (36.43%) | Memory: 164.8 MB (17.23%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -2,26 +2,23 @@ class Solution {
 public:
 int start=0;
 int maxlen=0;
-     void expand(string& s,int left,int right){
-        while(left>=0&&right<s.size()&&s[left]==s[right]){
-            left--;
-            right++;
-        }
-        int len=right-left-1;
-        if(len>maxlen){
-            maxlen=len;
-            start=left+1;
-        }
-     }
+void expand(string s,int i,int j){
+    while(i>=0&&j<s.size()&&s[i]==s[j]){
+        i--;
+        j++;
+         
+    }
+    
+   if(j-i-1>maxlen){
+    maxlen=j-i-1;
+     start=i+1;
+   }
+}
     string longestPalindrome(string s) {
-        //ya to even len ki palindrome ban sakta hai
-        // ya to odd len ki palindrome ban sakta hai
-        // we have to check for each char
+        // we have to expand around the center
         for(int i=0;i<s.size();i++){
-            // odd
-            // even
-            expand(s,i,i);
             expand(s,i,i+1);
+            expand(s,i,i);
         }
         return s.substr(start,maxlen);
     }

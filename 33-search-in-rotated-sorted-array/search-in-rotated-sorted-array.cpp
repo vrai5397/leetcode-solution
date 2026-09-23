37b8e4bf5104361e8b9d148bce2c1482f70a0a7e
vrai5397 <vrai5397@gmail.com>
2026-09-24 00:43:17 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (37.56%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        // roted array will alwyas have one side sorted
-        int n=nums.size();
         int lo=0;
-        int hi=n-1;
+        int hi=nums.size()-1;
         while(lo<=hi){
-            int mid=lo+(hi-lo)/2;
+             int mid=lo+(hi-lo)/2;
             if(nums[mid]==target)
             return mid;
             else if(nums[mid]>=nums[lo]){
-                // left half is sorted
-                if(nums[lo]<=target&&target<nums[mid])
+                // first phase is sorted
+                if(target>=nums[lo]&&target<nums[mid])
                 hi=mid-1;
                 else
                 lo=mid+1;
             }
             else{
-                // right half is sorted
-                 if(nums[mid]<target&&target<=nums[hi])
+                //second pahse is sorted
+                  if(target>nums[mid]&&target<=nums[hi])
                 lo=mid+1;
                 else
                 hi=mid-1;
             }
         }
-
         return -1;
     }
 };

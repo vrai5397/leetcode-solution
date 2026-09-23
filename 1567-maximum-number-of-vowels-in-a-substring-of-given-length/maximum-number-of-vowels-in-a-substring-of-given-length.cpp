9a80fed8e25f570e15b80ca422043379a4b7d1d9
vrai5397 <vrai5397@gmail.com>
2026-09-23 15:00:43 +0530

    Time: 11 ms (21.82%) | Memory: 13.3 MB (26.86%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
-bool isvowel(char ch){
-    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
-    return true;
-
-    return false;
-}
     int maxVowels(string s, int k) {
-        // substring of length k
-        // it can question of sliding window
+        // fixed size sliding window
         int i=0;
         int j=0;
-        int count=0;
-        int maxcount=0;
+      int count=0;
+      int maxcount=0;
         while(j<s.size()){
-           if(isvowel(s[j]))
-           count++;
-           while(j-i+1>k){
-              if(isvowel(s[i]))
-              count--;
-              i++;
-           }
-           if(j-i+1==k)
-           maxcount=max(maxcount,count);
-           j++;
+            if(s[j]=='a'||s[j]=='e'||s[j]=='i'||
+            s[j]=='o'||s[j]=='u')
+            count++;
+            while(j-i+1>k){
+                if(s[i]=='a'||s[i]=='e'||s[i]=='i'||
+            s[i]=='o'||s[i]=='u')
+            count--;
+             i++;
+            }
+            if(j-i+1==k)
+            maxcount=max(maxcount,count);
+            j++;
         }
         return maxcount;
     }

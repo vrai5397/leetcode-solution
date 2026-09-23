ae5a69db45cd40d2e823586b3a666397756c9507
vrai5397 <vrai5397@gmail.com>
2026-09-23 15:14:17 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (70.45%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-bool count(vector<int>& freq){
+bool solve(vector<int>& freq){
     for(auto x:freq){
         if(x!=0)
         return false;
@@ -8,22 +8,24 @@ bool count(vector<int>& freq){
     return true;
 }
     bool checkInclusion(string s1, string s2) {
+        // we have to find s1 size window
+        if(s2.size()<s1.size())
+        return false;
         int k=s1.size();
-        vector<int> freq(26,0);
-        for(int i=0;i<s1.size();i++){
-            freq[s1[i]-'a']++;
-        }
         int i=0;
         int j=0;
+        vector<int> freq(26,0);
+        for(auto x:s1)
+        freq[x-'a']++;
         while(j<s2.size()){
-           freq[s2[j]-'a']--;
+            freq[s2[j]-'a']--;
             while(j-i+1>k){
-                freq[s2[i]-'a']++;
-                i++;
+               freq[s2[i]-'a']++;
+               i++;
             }
-           if(j-i+1==k&& count(freq))
-           return true;
-           j++;
+            if(j-i+1==k&& solve(freq))
+            return true;
+            j++;
         }
         return false;
     }

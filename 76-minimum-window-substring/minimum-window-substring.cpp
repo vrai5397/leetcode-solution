3212b632d83f4358291dcf5f52702ee4899288d0
vrai5397 <vrai5397@gmail.com>
2026-09-23 11:10:38 +0530

    Time: 17 ms (27.07%) | Memory: 11.4 MB (64.73%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-int freqcount(unordered_map<char,int>& mp){
+bool solve(unordered_map<char,int>& mp){
     for(auto x:mp){
         if(x.second>0)
         return false;
@@ -8,29 +8,32 @@ int freqcount(unordered_map<char,int>& mp){
     return true;
 }
     string minWindow(string s, string t) {
-       unordered_map<char,int> mp;
-       for(int i=0;i<t.size();i++)
-       mp[t[i]]++;
+        if(s.size()<t.size())
+        return "";
        int i=0;
        int j=0;
-       int minlen=1e9;
+       int minlen=INT_MAX;
        int start=0;
-       while(j<s.size()){
-          mp[s[j]]--;
-          while(freqcount(mp)){
-             if(j-i+1<minlen){
-                start=i;
-                minlen=j-i+1;
-             }
-             mp[s[i]]++;
-            //  if(mp[s[i]]==0)
-            //  mp.erase(s[i]);
-             i++;
-          }
-j++;
+       unordered_map<char,int> mp;
+       for(auto x:t){
+          mp[x]++;
        }
-if(minlen==1e9)
-return "";
-return s.substr(start,minlen);
+       while(j<s.size()){
+           mp[s[j]]--;
+           while(solve(mp)){
+              if(j-i+1<minlen){
+                 minlen=j-i+1;
+                 start=i;
+              }
+              mp[s[i]]++;
+              if(mp[s[i]]==0)
+              mp.erase(s[i]);
+              i++;
+           }
+           j++;
+       }
+       if(minlen==INT_MAX)
+       return "";
+       return s.substr(start,minlen);
     }
 };

fd10f03c046591ac787d3446d556617e596d23fa
vrai5397 <vrai5397@gmail.com>
2026-09-24 01:45:33 +0530

    Time: 7 ms (69.31%) | Memory: 23 MB (58.44%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
+bool cansolve(vector<int>& arr,int speed,int h){
+    long long time=0;
+    for(auto x:arr){
+        if(x%speed==0)
+        time+=x/speed;
+        else
+        time+=(x/speed)+1;
+    }
+    if(time<=h)
+    return true;
+    return false;
+}
     int minEatingSpeed(vector<int>& piles, int h) {
-        // int hi=max of piles
-        // lo=1
-        int hi=INT_MIN;
-        for(int i=0;i<piles.size();i++)
-        hi=max(hi,piles[i]);
-
+        // hi=mid-1
+        // lo=mid+1;
         int lo=1;
+        int hi=INT_MIN;
+        for(auto x:piles)
+        hi=max(hi,x);
         while(lo<=hi){
             int mid=lo+(hi-lo)/2;
-            long long hours=0;
-            for(auto x:piles){
-                // mid is speed
-               if(x%mid!=0){
-                  hours+=x/mid;
-                  hours++;
-               }
-               else
-               hours+=x/mid;
-            }
-            if(hours<=h)
-                hi=mid-1;
+            if(cansolve(piles,mid,h))
+            hi=mid-1;
             else
             lo=mid+1;
-
         }
-return lo;
+        return lo;
     }
 };

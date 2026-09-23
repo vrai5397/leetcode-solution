1b52a7b8a2e49dd0a3c1bdd6c63ff354c8969f44
vrai5397 <vrai5397@gmail.com>
2026-09-23 15:48:58 +0530

    Time: 0 ms (100.00%) | Memory: 69.6 MB (70.28%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        //largest subarray with k zero
+        int countzeros=0;
         int i=0;
         int j=0;
-        int count=0;
-        int maxsize=0;
+        int maxlen=0;
         while(j<nums.size()){
             if(nums[j]==0)
-            count++;
-            while(count>k){
-              if(nums[i]==0)
-              count--;
-              i++;
+            countzeros++;
+            while(countzeros>k){
+                if(nums[i]==0)
+                countzeros--;
+                i++;
             }
-            maxsize=max(maxsize,j-i+1);
+            maxlen=max(maxlen,j-i+1);
             j++;
         }
-        return maxsize;
+        return maxlen;
     }
 };
